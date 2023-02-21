054c9939bab919c1a3d146c45dff43dfc1e0cfd7
Yasser Angel <yasserangelgm@gmail.com>
2023-02-21 00:35:21 -0600

    feat: Add logging middleware. Add CORS an cors options

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
-const express = require("express");
-const path = require("path");
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const { logger } = require('./middlewares/logger');
+const errorHandler = require('./middlewares/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 
-require("dotenv").config();
+const PORT = process.env.PORT || 3500;
 
 const app = express();
 
 /* MIDDLEWARES */
-app.use("/", express.static(path.join(__dirname, "/public")));
-app.use("/", require("./routes/root"));
-app.all("*", (req, res) => {
+app.use(logger);
+app.use(cors(corsOptions));
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/root'));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.all('*', (req, res) => {
   res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ message: "404 Not Found" });
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' });
   } else {
-    res.type("text").send("404 Not Found");
+    res.type('text').send('404 Not Found');
   }
 });
-const PORT = process.env.PORT || 3500;
 
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
